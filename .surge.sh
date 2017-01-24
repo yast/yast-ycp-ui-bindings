858fdb4925fbe14d57f0625f17cd51b6476c253e
Josef Reidinger <jreidinger@suse.cz>
2017-01-24 16:29:15 +0100

    do not disclosure token

--- .surge.sh
+++ .surge.sh
@@ -4,8 +4,6 @@
 # Manual uploading require credentials, so only travis can do this.
 # For more info see https://surge.sh/
 
-set -x
-
 if [ "$TRAVIS_EVENT_TYPE" = "push" -a "$TRAVIS_BRANCH" = "master" ]; then
   docker run -it -e TRAVIS=1 -e TRAVIS_JOB_ID="$TRAVIS_JOB_ID" \
     -e SURGE_LOGIN="$SURGE_LOGIN" -e SURGE_TOKEN="$SURGE_TOKEN" \
