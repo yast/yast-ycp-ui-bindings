c23abc0800f5c41af4a2809c86e6f0939673c4eb
Josef Reidinger <jreidinger@suse.cz>
2017-01-24 15:04:03 +0100

    fix surge script

--- .surge.sh
+++ .surge.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-[ "$TRAVIS_EVENT_TYPE" = "push" -a "$TRAVIS_BRANCH" = "master" ] && docker run -it -e TRAVIS=1 -e TRAVIS_JOB_ID="$TRAVIS_JOB_ID" -e SURGE_LOGIN="$SURGE_LOGIN" -e SURGE_TOKEN="$SURGE_TOKEN" yast-ycp-ui-bindings-image /bin/bash -c "zypper  in -y npm doxygen; npm install --global surge; rake doc; surge --project ./autodoc/html --domain yast-ui-bindings.surge.sh"
+set -x
+
+if [ "$TRAVIS_EVENT_TYPE" = "push" -a "$TRAVIS_BRANCH" = "master" ]; then
+  docker run -it -e TRAVIS=1 -e TRAVIS_JOB_ID="$TRAVIS_JOB_ID" -e SURGE_LOGIN="$SURGE_LOGIN" -e SURGE_TOKEN="$SURGE_TOKEN" yast-ycp-ui-bindings-image /bin/bash -c "zypper  in -y npm doxygen; npm install --global surge; rake doc; surge --project ./autodoc/html --domain yast-ui-bindings.surge.sh"
+else
+  echo "not published"
+fi
