c5dca3302e9f3e0b2dd01bce271e5951b6808092
Stefan Hundhammer <shundhammer@suse.de>
2019-11-06 12:40:12 +0100

    New simplistic example

---
+++ examples/CustomStatusItemSelector1.rb
@@ -0,0 +1,54 @@
+# encoding: utf-8
+
+module Yast
+  class ExampleClient < Client
+    def main
+      Yast.import "UI"
+
+      UI.OpenDialog(
+	VBox(
+	  CustomStatusItemSelector(
+	    Id(:pizza),
+	    [
+	      # Icon, NCursesIndicator, NextStatus
+	      ["checkbox-off", "[ ]", 1],
+	      ["checkbox-on",  "[x]", 0]
+	    ],
+	    [
+	      # Notice no item IDs, so we'll get the item label as the result.
+	      # Even the descriptions are optional.
+	      Item("Pizza Margherita",	     "Very basic with just tomatoes and cheese"),
+	      Item("Pizza Capricciosa",	     "Ham and vegetables"		       ),
+	      Item("Pizza Funghi",	     "Mushrooms"			       ),
+	      Item("Pizza Prosciutto",	     "Ham"				       ),
+	      Item("Pizza Quattro Stagioni", "Different toppings in each quarter"      ),
+	      Item("Calzone",		     "Folded over"			       )
+	    ]
+	   ),
+	  PushButton("&OK")
+	)
+      )
+      UI.UserInput
+
+      # Fetch the result as long as the widget still exists, i.e. BEFORE UI.CloseDialog
+
+      result = UI.QueryWidget(:pizza, :SelectedItems).join(", ")
+      result = "(nothing)" if result.empty?
+      UI.CloseDialog
+
+      # Show the result in a pop-up dialog
+      UI.OpenDialog(
+	VBox(
+	  Label("\n  Selected:\n\n  #{result}  \n"),
+	  PushButton("&OK")
+	)
+      )
+      UI.UserInput
+      UI.CloseDialog
+
+      nil
+    end
+  end
+end
+
+Yast::ExampleClient.new.main
