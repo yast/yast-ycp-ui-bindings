bb911ea00a4e2d0729f9779835cece379c371c4f
Stefan Hundhammer <shundhammer@suse.de>
2022-03-15 11:28:48 +0100

    Modernized outdated example

--- examples/Wizard2.rb
+++ examples/Wizard2.rb
@@ -1,50 +1,70 @@
 # encoding: utf-8
-
-# Example of using the Wizard widget.
 #
-# Note: YCP applications are discouraged from using the Wizard widget directly.
+# Wizard example with steps.
+#
+# Note: Ruby applications are discouraged from using the Wizard widget directly.
 # Use the Wizard module instead.
+#
+
 module Yast
-  class Wizard2Client < Client
+  class WizardClient < Client
+    include Yast::Logger
+    YAST_ICON = "/usr/share/icons/hicolor/scalable/apps/yast.svg".freeze
+    
     def main
       Yast.import "UI"
-      if !UI.HasSpecialWidget(:Wizard)
-        Builtins.y2error(
-          "This works only with UIs that provide the wizard widget!"
-        )
-        return
+      
+      return unless ensure_wizard_widget
+      
+      UI.OpenDialog(Opt(:defaultsize), wizard_dialog)
+      set_up_wizard
+      event_loop
+      UI.CloseDialog
+      nil
+    end
+
+    def event_loop
+      while true
+        event = UI.WaitForEvent
+        log.info("Got event: #{event}")
+        break if event["ID"] == :abort
+
+        display_event(event)
       end
+    end
 
-      @help_text = "<p>This is a help text.</p>" +
-        "<p>It should be helpful.</p>" +
-        "<p>If it isn't helpful, it should rather not be called a <i>help text</i>.</p>"
+    def ensure_wizard_widget
+      return true if UI.HasSpecialWidget(:Wizard)
 
-      UI.OpenDialog(
-        Opt(:defaultsize),
-        Wizard(
-          Opt(:stepsEnabled),
-          :back,
-          "&Back",
-          :abort,
-          "Ab&ort",
-          :next,
-          "&Next"
-        )
+      msg = "FATAL: This works only with UIs that provide the wizard widget!"
+      log.error(msg)
+      puts(msg)
+      false
+    end
+
+    def wizard_dialog
+      Wizard(
+        Opt(:stepsEnabled),
+        :back, "&Back",
+        :abort, "Ab&ort",
+        :next, "&Next"
       )
+    end
 
-      # UI::DumpWidgetTree();
+    def set_up_wizard
+      UI.WizardCommand(term(:SetDialogIcon, YAST_ICON))
+      UI.WizardCommand(term(:SetDialogHeading, "Welcome to the YaST2 installation"))
+      UI.WizardCommand(term(:SetHelpText, help_text))
+      add_wizard_steps
+    end
 
-      UI.WizardCommand(
-        term(
-          :SetDialogIcon,
-          "/usr/share/YaST2/theme/current/icons/22x22/apps/YaST.png"
-        )
-      )
-      UI.WizardCommand(
-        term(:SetDialogHeading, "Welcome to the YaST2 installation")
-      )
-      UI.WizardCommand(term(:SetHelpText, @help_text))
+    def help_text
+      "<p>This is a help text.</p>" +
+      "<p>It should be helpful.</p>" +
+      "<p>If it isn't helpful, it should rather not be called a <i>help text</i>.</p>"
+    end
 
+    def add_wizard_steps
       UI.WizardCommand(term(:AddStepHeading, "Base Installation"))
       UI.WizardCommand(term(:AddStep, "Language", "lang"))
       UI.WizardCommand(term(:AddStep, "Installation Settings", "proposal"))
@@ -59,44 +79,29 @@ module Yast
       UI.WizardCommand(term(:AddStep, "Release Notes", "rel_notes"))
       UI.WizardCommand(term(:AddStep, "Device Configuration", "hw_proposal"))
       UI.WizardCommand(term(:UpdateSteps))
-
-      if false
-        UI.WizardCommand(term(:SetAbortButtonLabel, "&Cancel"))
-        UI.WizardCommand(term(:SetBackButtonLabel, ""))
-        UI.WizardCommand(term(:SetNextButtonLabel, "&Accept"))
-      end
-
+      
       UI.WizardCommand(term(:SetCurrentStep, "net"))
-
-      while true
-        @event = UI.WaitForEvent
-
-        Builtins.y2milestone("Got event: %1", @event)
-
-        break if Ops.get(@event, "ID") == :abort
-
-        @serial = Ops.get_integer(@event, "EventSerialNo", 0)
-        @type = Ops.get_string(@event, "EventType", "")
-        @id = Ops.get_symbol(@event, "ID", :nil)
-
-
-        UI.ReplaceWidget(
-          Id(:contents),
-          VBox(
-            Heading("Caught event:"),
-            VSpacing(0.5),
-            Label(Ops.add("Serial No: ", Builtins.tostring(@serial))),
-            Label(Ops.add("Type: ", @type)),
-            Label(Ops.add("ID: ", Builtins.tostring(@id)))
-          )
-        )
-      end
-
-      UI.CloseDialog
-
-      nil
     end
+  end
+
+  def display_event(event)
+    serial = event["EventSerialNo"]
+    type = event["EventType"]
+    id = event["ID"]
+
+    UI.ReplaceWidget(
+      Id(:contents),
+      HVSquash(
+        VBox(
+          Heading("Caught event:"),
+          VSpacing(0.5),
+          Left(Label("Type: #{type}")),
+          Left(Label("ID: #{id}")),
+          Left(Label("Serial No: #{serial}"))
+        )
+      )
+    )
   end
 end
 
-Yast::Wizard2Client.new.main
+Yast::WizardClient.new.main
