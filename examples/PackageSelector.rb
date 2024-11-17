bf97801ed81fe0105aa07de457d9a53553aa931d
Stefan Hundhammer <shundhammer@suse.de>
2024-11-17 13:19:11 +0100

    Fixed PackageSelector example to actually load packages

--- examples/PackageSelector.rb
+++ examples/PackageSelector.rb
@@ -6,23 +6,17 @@ module Yast
     def main
       Yast.import "UI"
       Yast.import "Pkg"
-      # Pkg::SourceCreate( "http://dist.suse.de/install/SLP/SUSE-10.1-Beta7/i386/CD1/", "" );
-      # Pkg::SourceCreate( "http://dist.suse.de/install/SLP/SUSE-10.0-RC4/i386/CD1/", "" );
-      # Pkg::SourceCreate( "file:/srv/10.1-i386/CD1/", "" );
-      Pkg.SourceCreate("file:/srv/10.1-i386/DVD1/", "")
-      # Pkg::SourceCreate( "file:/srv/sles-10-i386/CD1/", "" );
 
-      if true
-        Pkg.TargetInit(
-          "/", # installed system
-          false
-        ) # don't create a new RPM database
-      end
+      Pkg.TargetInitialize("/")
+      Pkg.TargetLoad
+      Pkg.SourceRestore
+      Pkg.SourceLoad
 
       UI.OpenDialog(
         Opt(:defaultsize),
-        PackageSelector(Id(:selector), "/dev/fd0")
+        PackageSelector(Id(:selector))
       )
+
       @input = UI.RunPkgSelection(Id(:selector))
       UI.CloseDialog
 
